ae7af6474cae054556f1ff3446c50f85e1ed98af
Muhammad Hussain Javed <k213584@nu.edu.pk>
2024-01-13 00:14:44 +0500

    Copy pasted config from WSL

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed  = { "lua_ls", "bashls", "clangd", "pyright" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.bashls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+    end
+  }
+}
